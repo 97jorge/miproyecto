cf9c87c0a819c508c6921efee61c6bda647e1f90
97jorge <jgonzalezb@unah.hn>
2023-06-22 16:17:31 -0600

    Se agrega el proyecto

---
+++ resources/views/home.blade.php
@@ -0,0 +1,39 @@
+
+@extends('adminlte::page')
+
+@section('title', 'Dashboard')
+
+@section('content_header')
+    <h1>Dashboard</h1>
+@stop
+
+@section('content')
+
+<section>
+
+    <ul>
+        <li>jorge</li>
+        <li>enrique</li>
+        <li>gonzalez</li>
+    </ul>
+
+</section>
+<section> <p>Welcome to this beautiful admin panel.</p></section>
+   <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga consequatur unde 
+    blanditiis sunt alias minima modi placeat non voluptas facere officia, sequi sint ducimus illo itaque id animi sit distinctio.
+   </p>
+
+
+
+
+@stop
+
+
+
+@section('css')
+    <link rel="stylesheet" href="/css/admin_custom.css">
+@stop
+
+@section('js')
+    <script> console.log('Hi!'); </script>
+@stop
